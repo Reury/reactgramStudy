fc2ae0255f57a41058ec01a7cf1f060f77ac418a
Reury <reuryaraujo@gmail.com>
2024-05-13 15:10:23 -0300

    started fron-end development

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+// router
+import { BrowserRouter,Routes,Route,Navigate} from 'react-router-dom';
+// pages
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+// components
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
+          </Routes>
+      </div>
+        <Footer></Footer>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import "./NavBar.css"
+
+// Components
+import { NavLink,Link } from "react-router-dom"
+import {BsSearch,BsHouseDoorFill,BsFillPerson,BsFillCameraFill} from 'react-icons/bs'
+
+const NavBar = () => {
+  return (
+    <nav id="nav">
+      <Link to="/">ReactGram</Link>
+      <form >
+        <BsSearch></BsSearch>
+        <input type="text" name="search" id="search" />
+      </form>
+      <ul id="nav-links">
+        <NavLink to="/"><BsHouseDoorFill></BsHouseDoorFill></NavLink>
+        <NavLink to="/login">Entrar</NavLink>
+        <NavLink to="/register">Cadastrar</NavLink>       
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
