f9910f587cdc8c561ec96eacd56032ff249dba96
Reury <reuryaraujo@gmail.com>
2024-05-21 10:06:32 -0300

    adicionado config para react-redux

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,1 +1,8 @@
-export const api = "http://localhost:5001/api"
+export const api = "http://localhost:5001/api";
+export const upload = "http://localhost:5001/uploads";
+
+export const requestConfig = (method,data,token=null,image=null)=>{
+
+}
+
+
